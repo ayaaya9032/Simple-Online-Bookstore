f712911fdf5ca8893bcc4fd5c90db1adf3b21f05
HuZiii <Hzj0627@icloud.com>
2020-08-22 12:16:18 +0800

    Init project.

---
+++ back-end/src/test/java/com/huziii/demo/project/simple/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huziii.demo.project.simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
